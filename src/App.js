61dc4d03c072059c5efdb45767a018ece639d936
Barrerse <sebastianbarrera@live.com>
2023-03-14 19:22:09 -0400

    starting search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import Search from './components/Search.js';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       {/* nav bar */}
       <Navbar />
       {/* categories */}
+      <Search />
       {/* recomended */}
       {/* offers */}
       {/* browse */}  
