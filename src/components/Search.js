e7122250a4b61473556b624bda477db6a3efec15
Barrerse <sebastianbarrera@live.com>
2023-03-14 20:09:18 -0400

    finished front end for Search section

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="gradient mx-[2.5rem] pl-4 pr-2 py-[0.1rem] mt-8 flex items-center justify-between">   
+        <ul className="flex itmes-cente py-1.5 text-[14px] text-[white] gap-10 ">
+            <li>
+                <p>Games</p>
+            </li>
+            <li>
+                <p>Kickstarters</p>
+            </li>
+            <li>
+                <p>Discussion</p>
+            </li>
+            <li>
+                <p>Deals</p>
+            </li>
+            <li>
+                <p>News</p>
+            </li>
+            <li>
+                <p>Coming Soon</p>
+            </li>
+        </ul>
+        <input type="search"
+        placeholder="Search"
+        className="pl-4 rounded placeholder:text-black" />
+    </div>
   )
 }
 
