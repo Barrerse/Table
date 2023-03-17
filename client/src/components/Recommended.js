14d8e3981feb661ba91a4566439bbd0dae511f74
Barrerse <sebastianbarrera@live.com>
2023-03-16 20:11:27 -0400

    changed mobile styling of home page

--- client/src/components/Recommended.js
+++ client/src/components/Recommended.js
@@ -18,65 +18,68 @@ const Recomended = () => {
   };
 
   return (
-    <div className="mx-[2rem] mt-[2rem] text-white text-[14px] overflow-hidden">
+    <div className="mx-4 mt-4 text-white text-sm md:text-base overflow-hidden">
       {/* Title */}
-      <p>FEATURED</p>
+      <p className="text-2xl font-bold">FEATURED</p>
 
       {/* Featured */}
-      <div className="h-full md:h-[24rem] w-full flex flex-col  md:flex-row pt-3">
-        <div className="w-full md:w-[62%] h-full bg-red-400 flex ">
-        <img src={mainImage} alt="Board game main" className="object-cover w-full transition-image" />
+      <div className="flex flex-col md:flex-row md:pt-3">
+        <div className="w-full md:w-3/5 h-full bg-red-400">
+          <img
+            src={mainImage}
+            alt="Board game main"
+            className="object-cover w-full h-full transition-image"
+          />
         </div>
-        <div className="bg-[#0f1922] h-full w-full md:w-[38%] flex  flex-col justify-between ">
-          <div className="  flex flex-col  items-center">
-            <p className="text-[30px] mt-3">Name of Game</p>
-            <div className="px-4 w-full h-[24rem] md:h-[15rem] pt-3">
-              <div className="h-[40%]   w-full flex pb-1 ">
+        <div className="bg-[#0f1922] w-full md:w-2/5 flex flex-col justify-between">
+          <div className="flex flex-col items-center justify-center h-full">
+            <p className="text-4xl mt-3">Name of Game</p>
+            <div className="px-4 w-full h-full pt-3">
+              <div className="h-1/2 w-full flex pb-1">
                 <img
                   src={gaming1}
                   alt=""
-                  className="object-cover w-[50%] pr-1 "
+                  className="object-cover w-1/2 pr-1"
                   onMouseOver={() => handleImageHover(gaming1)}
                   onMouseOut={handleImageMouseOut}
                 />
                 <img
                   src={gaming2}
                   alt=""
-                  className="object-cover w-[50%] pl-1"
+                  className="object-cover w-1/2 pl-1"
                   onMouseOver={() => handleImageHover(gaming2)}
                   onMouseOut={handleImageMouseOut}
                 />
               </div>
-              <div className="h-[40%]  w-full flex pt-1 ">
+              <div className="h-1/2 w-full flex pt-1">
                 <img
                   src={gaming3}
                   alt=""
-                  className="object-cover w-[50%] pr-1"
+                  className="object-cover w-1/2 pr-1"
                   onMouseOver={() => handleImageHover(gaming3)}
                   onMouseOut={handleImageMouseOut}
                 />
                 <img
                   src={gaming4}
                   alt=""
-                  className="object-cover w-[50%] pl-1"
+                  className="object-cover w-1/2 pl-1"
                   onMouseOver={() => handleImageHover(gaming4)}
                   onMouseOut={handleImageMouseOut}
                 />
               </div>
-              <div className=" flex flex-col items-center md:items-start  ">
-                <p className="text-[22px] pt-4">Buy Now</p>
-                <div className="bg-[#8cc414] w-[6rem] rounded-[0.5rem] mt-1">
-                  <p className="text-[15px] text-center">Discount</p>
-                </div>
-              </div>
             </div>
           </div>
-          <div className="flex items-center justify-center md:justify-between pr-4 gap-4 md:gap-0 -mt-4 md:mt-0 pb-2 md:pb-0">
-            <div className="pl-4 pb-2 ">
+          <div className="flex items-center justify-between px-4 py-2">
+            <div>
               {/* Todo: Add price using API  */}
-              <p className="text-[20px] md:text-[12px]">PRICE</p>
+              <p className="text-lg font-bold">PRICE</p>
             </div>
-            <TbApps className="text-[25px] md:text-[19px]   mb-[10px]" />
+            <div className="flex items-center">
+              <div className="bg-green-500 rounded-md py-1 px-3 mr-2">
+                <p className="text-sm font-bold">Discount</p>
+              </div>
+              <TbApps className="text-4xl mb-1" />
+            </div>
           </div>
         </div>
       </div>
