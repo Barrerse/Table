be9f8a6860c3d7362ac96f234de0edc45de0a7b3
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-20 19:41:35 -0400

    Added routes to main add.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
-import Login from './components/Login.js';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Home from './pages/Home.js';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="bg-[#1b2838] h-screen">
     
-      <Home />
-      {/* <Login /> */}
-    
-    <div className=""></div>
-    </div>
+    <Home />
+    {/* <Login /> */}
+  
+  <div className=""></div>
+  </div>
+    </ApolloProvider>
+
   );
 }
 
