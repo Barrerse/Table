211fb813e2edbafcc0f5298c9ab542b7570cd78b
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-20 19:24:10 -0400

    Updated files for models using users and games saved

--- server/models/game.js
+++ server/models/game.js
@@ -1,20 +1,18 @@
 const { Schema } = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedgames` array in User.js
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedGames` array in User.js
 const gameSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
   description: {
     type: String,
     required: true,
   },
-  // saved game id from Googlegames
+  // saved game id from board game API
   gameId: {
     type: String,
     required: true,
+  },
+  price: {
+    type: String,
   },
   image: {
     type: String,
