a843021c2f63bf4ad32d8894f45c9209daa27674
Barrerse <sebastianbarrera@live.com>
2023-03-21 20:57:47 -0400

    finished search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,12 +3,19 @@ import { FiSearch } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { searchGamesAPI } from '../utils/APIQ';
 
-const truncateWords = (text, limit) => {
-  const words = text.split(' ');
-  if (words.length > limit) {
-    return words.slice(0, limit).join(' ') + '...';
+const truncateSentences = (text, limit) => {
+  if (text.length <= limit) {
+    return text;
   }
-  return text;
+
+  const truncatedText = text.slice(0, limit);
+  const lastPeriodIndex = truncatedText.lastIndexOf('.');
+  
+  if (lastPeriodIndex === -1) {
+    return truncatedText.trim() + '...';
+  }
+
+  return truncatedText.slice(0, lastPeriodIndex + 1);
 };
 
 const Search = () => {
@@ -28,7 +35,6 @@ const Search = () => {
       console.log(error);
     }
   };
-
   return (
     <div>
       <div className="gradient mx-[2.5rem] pl-4 pr-2 py-[0.1rem] mt-8 sm:flex sm:items-center sm:justify-between" style={{ backgroundColor: '#316282' }}>
@@ -65,16 +71,16 @@ const Search = () => {
         </div>
       </div>
       {showResults && searchedGames.length > 0 && (
-        <div className="search-results-container w-full bg-white mt-4 py-4 px-4 rounded-md shadow-md">
-          <h2 className="mb-4">Search Results</h2>
+        <div className="search-results-container w-full bg-[#1b2838] mt-4 py-4 px-4 rounded-md shadow-md">
+          <h2 className="mb-4 text-white">Search Results</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {searchedGames.map((game) => (
               <div className="card rounded-lg overflow-hidden shadow-md" key={game.id}>
                 <img src={game.images.medium} className="card-img-top w-full h-64 object-cover" alt={game.name} />
                 <div className="card-body p-4">
-                  <h5 className="card-title text-lg font-semibold mb-2">{game.name}</h5>
-                  <p className="card-text text-sm text-gray-700 mb-4">{truncateWords(game.description_preview, 20)}</p>
-                  <Link to={`/game/${game.id}`} className="btn btn-primary">
+                  <h5 className="card-title text-lg font-semibold mb-2 text-white">{game.name}</h5>
+                  <p className="card-text text-sm text-gray-300 mb-4">{truncateSentences(game.description_preview, 100)}</p>
+                  <Link to={`${game.url}`} className="btn btn-primary text-white text-decoration-line: underline" target="_blank">
                     View Details
                   </Link>
                 </div>
