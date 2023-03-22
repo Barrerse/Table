548043ebaf24e622ee15d5b7c29cc8f2e6e1234b
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-21 21:11:17 -0400

    updated code to user.js model

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,43 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
-  },
-  lastname: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    max: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
 
-    required: true,
-    min: 5,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    ocupation: String,
+    viewedProfile: Number,
+    impressions: Number,
   },
-  picturePath: {
-    type: String,
-    default: "",
-  },
-  friends: {
-    type: Array,
-    default: [],
-  },
-  location: String,
-  ocupation: String,
-  viewedProfile: Number,
-  impressions: Number,
-});
+  { timestamps: true }
+);
