3970cbd870381439bf9b1e798a6bd7eb56891b80
Barrerse <sebastianbarrera@live.com>
2023-03-21 20:35:43 -0400

    Added search function cards, made it so the words are limited

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,6 +3,14 @@ import { FiSearch } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { searchGamesAPI } from '../utils/APIQ';
 
+const truncateWords = (text, limit) => {
+  const words = text.split(' ');
+  if (words.length > limit) {
+    return words.slice(0, limit).join(' ') + '...';
+  }
+  return text;
+};
+
 const Search = () => {
   const [searchInput, setSearchInput] = useState('');
   const [searchedGames, setSearchedGames] = useState([]);
@@ -59,13 +67,13 @@ const Search = () => {
       {showResults && searchedGames.length > 0 && (
         <div className="search-results-container w-full bg-white mt-4 py-4 px-4 rounded-md shadow-md">
           <h2 className="mb-4">Search Results</h2>
-          <div className="card-columns">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {searchedGames.map((game) => (
-              <div className="card" key={game.id}>
-                <img src={game.images.medium} className="card-img-top" alt={game.name} />
-                <div className="card-body">
-                  <h5 className="card-title">{game.name}</h5>
-                  <p className="card-text">{game.description_preview}</p>
+              <div className="card rounded-lg overflow-hidden shadow-md" key={game.id}>
+                <img src={game.images.medium} className="card-img-top w-full h-64 object-cover" alt={game.name} />
+                <div className="card-body p-4">
+                  <h5 className="card-title text-lg font-semibold mb-2">{game.name}</h5>
+                  <p className="card-text text-sm text-gray-700 mb-4">{truncateWords(game.description_preview, 20)}</p>
                   <Link to={`/game/${game.id}`} className="btn btn-primary">
                     View Details
                   </Link>
