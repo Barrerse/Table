4b608273f458c9d76769221ebfa641aa3cab15e2
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-22 02:05:28 -0400

    Added links to profile page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,19 +1,31 @@
 import Navbar from "../components/Navbar";
 import { useContext } from "react";
 import { UserContext } from "../utils/UserContext";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 
 export default function Account() {
-  const { user } = useContext(UserContext);
+  const { ready, user } = useContext(UserContext);
 
-  if (!user) {
+  if (!ready) return "Loading...";
+
+  if (ready && !user) {
     return <Navigate to={"/login"} />;
   }
 
   return (
     <div>
       <Navbar />
-      <div>account pages for {user.name}</div>
+      <nav className="w-full flex justify-center mt-8 gap-4">
+        <Link className="p-2 px-6 bg-gray-300 rounded-full" to={"/account"}>
+          My Profile
+        </Link>
+        <Link className="p-2 px-6 text-white" to={"/account/games"}>
+          My Games
+        </Link>
+        <Link className="p-2 px-6 text-white" to={"/account/comments"}>
+          My Comments
+        </Link>
+      </nav>
     </div>
   );
 }
